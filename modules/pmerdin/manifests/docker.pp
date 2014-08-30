a0d5d64edf8c186603e87ce73991f297184988ae
root <root@onmetal-server-01.localdomain>
2014-08-30 19:39:14 +0000

    docker setup for mesos

---
+++ modules/pmerdin/manifests/docker.pp
@@ -0,0 +1,30 @@
+# setup docker for mesos
+
+class pmerdin::docker () {
+  apt::source { 'docker.io':
+    location    => "https://get.docker.io/ubuntu",
+    #release     => $::lsbdistcodename,
+    release     => 'docker',
+    repos       => 'main',
+    key         => 'A88D21E9',
+    key_server  => 'keyserver.ubuntu.com',
+    include_src => false,
+  } ->
+  package{ 'lxc-docker':
+      ensure => latest,
+  } ->
+  file{ '/usr/local/bin/docker':
+    ensure => link,
+    target => '/usr/bin/docker.io',
+  }
+
+  package{ 'cgroup-bin':
+    ensure => latest,
+  } ->
+  file{ "${mesos::slave::conf_dir}/containerizers":
+    content => inline_template('docker,mesos'),
+    notify  => Service['mesos-slave'],
+  }
+
+
+}
