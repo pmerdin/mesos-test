65e898b427cd9df35d7158ae978750885a834ec5
root <root@onmetal-server-01.localdomain>
2014-08-30 13:08:39 +0000

    more packages

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,6 +1,6 @@
 # baremetal host setup
 
 apt-get update
-apt-get install mc ruby virtualbox virtualbox-dkms python 
+apt-get install mc ruby virtualbox virtualbox-dkms python git apt-file
 wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_x86_64.deb
 dpkg -i vagrant_1.6.3_x86_64.deb
