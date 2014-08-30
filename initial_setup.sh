203b38a3726a25d306e5b25568127f99d637e16b
root <root@onmetal-server-01.localdomain>
2014-08-30 14:04:11 +0000

    stuff needed for checks

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,6 +1,7 @@
 # baremetal host setup
 
 apt-get update
-apt-get install mc ruby virtualbox virtualbox-dkms python git apt-file
+apt-get install mc ruby virtualbox virtualbox-dkms python git apt-file python-pip
 wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_x86_64.deb
 dpkg -i vagrant_1.6.3_x86_64.deb
+pip install nagiosplugin
